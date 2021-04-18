29a4161aa623e53e93d8b6fbfbeeddc4501415ec
Hector <hector.ibarra@isibit.travel>
2021-04-17 20:07:29 -0500

    adding sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react'; 
+import React from "react";
+import { Content } from "./components/layout/Content";
+import { Header } from "./components/layout/Header";
 
 export const App = () => (
   <div className="App">
-    <header className="App-header">
-      <p>Hello from here</p>
-    </header>
+    <Header /> 
+    <Content/>
   </div>
 );
-
