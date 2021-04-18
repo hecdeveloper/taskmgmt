5a9a1e3b798e25cd9024303a833052149b8b5ebb
Hector <hector.ibarra@isibit.travel>
2021-04-17 20:58:47 -0500

    setuup fir deploy

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -3,17 +3,18 @@ import {
   FaChevron,
   FaInbox,
   FaRegCalendarAlt,
-  FaRaCalendar,
+  FaRegCalendar,
 } from "react-icons/fa";
 
 export const Sidebar = () => (
   <div className="sidebar" data-testid="sidebar">
     <ul className="sidebar_generic">
       <li>
-        {" "}
         <span><FaInbox/></span> <span>Inbox</span>
       </li>
-      <li>Today</li>
+      <li>
+        <span><FaRegCalendar/></span> <span>Today</span>
+      </li>
       <li>Next 7 days</li>
     </ul>
   </div>
